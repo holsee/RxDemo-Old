b44b81b58a2f0fbbcb12db519a72a78116b9ba6c
Steven Holdsworth <sholdsworth@gmail.com>
2010-07-09 15:36:49 +0100

    Included custom extension methods (Operators) for IObservables

---
+++ Holsee.RxLab/ObservableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Holsee.RxLab
+{
+    public static class ObservableExtensions
+    {
+        /// <summary>
+        /// Converts <paramref name="source"/> to IObservable of Timestamped of <typeparamref name="T"/>, 
+        /// performs <paramref name="onNext"/>, then converts <paramref name="source"/> back.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="onNext"></param>
+        /// <returns></returns>
+        public static IObservable<T> LogTimestampedValues<T>(this IObservable<T> source, Action<Timestamped<T>> onNext)
+        {
+            return source.Timestamp().Do(onNext).RemoveTimestamp();
+        }
+    }
+}
